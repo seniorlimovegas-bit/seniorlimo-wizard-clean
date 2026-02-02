560ae6984ec3c59ad6df12cde62c4f2cf980e365
seniorlimovegas-bit <seniorlimovegas@gmail.com>
2026-02-01 16:29:12 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
   reactStrictMode: true,
 };
 
