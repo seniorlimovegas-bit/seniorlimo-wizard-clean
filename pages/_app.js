a8e7bb6e21d16fac2adc62d9a25d5e6b2d16e576
seniorlimovegas-bit <seniorlimovegas@gmail.com>
2026-02-06 20:51:04 -0800

    Replace App component with API handler

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import '../styles/globals.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
 }
